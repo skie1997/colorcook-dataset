ae480491cee4129f0ac51aed4961082e5607c4ce
skie1997 <sijichen.idvx@gmail.com>
2021-10-24 23:50:47 +0800

    display colorset

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
+import { BulbOutlined, StockOutlined } from '@ant-design/icons';
 import './App.css';
+import { colorStyle } from './assets/palettes/colorStyle_all_2_ok';
+import ColorsetCard from './ColorsetCard';
+import SubMenu from 'antd/lib/menu/SubMenu';
+const { Header, Footer, Sider, Content } = Layout;
 
-function App() {
-  return (
+class App extends React.Component {
+  handleClick = (e: any) => {
+    console.log('e', e);
+  }
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className = 'header'>
+          <h1 className = 'header-title'>Palettes Gallery Of ColorCook</h1>
+          <h3 className = 'header-description'>
+            We collected graphic designs(illustrations) that depict scenarios of the identified domains and extracted corresponding color palettes from these
+            designs. We also validated the dataset from three aspects, including distinctiveness, aesthetics, and specificity. The
+            final dataset contains 115 manually curated palettes and is released to support the design of future data-driven color
+            authoring tools. 
+          </h3>
+        </Header>
+        <Content className = 'content'>
+          <div className = 'content-side'>
+            <Menu mode = "inline" openKeys = {['mode']} style = {{ height: '100%' }} onClick = {this.handleClick}>
+              <SubMenu key = "mode" title = "Mode">
+                <Menu.Item key = "dark" >dark</Menu.Item>
+                <Menu.Item key = "light" >light</Menu.Item>
+              </SubMenu>
+            </Menu>
+
+            <Menu mode = "inline" openKeys = {['rank']} style = {{ height: '100%' }} onClick = {this.handleClick}>
+              <SubMenu key = "rank" title = "Rank">
+                <Menu.Item key = "aesthetics" >aesthetics</Menu.Item>
+                <Menu.Item key = "distinctiveness" >distinctiveness</Menu.Item>
+              </SubMenu>
+            </Menu>
+
+            <Menu mode = "inline" openKeys = {['industry']} style = {{ height: '100%' }} onClick = {this.handleClick}>
+              <SubMenu key = "industry" title = "Industry">
+                <Menu.Item key = "business" >business</Menu.Item>
+                <Menu.Item key = "education" >education</Menu.Item>
+                <Menu.Item key = "healthcare" >healthcare</Menu.Item>
+                <Menu.Item key = "finance" >finance</Menu.Item>
+                <Menu.Item key = "manufacturing" >manufacturing</Menu.Item>
+                <Menu.Item key = "social" >social media</Menu.Item>
+                <Menu.Item key = "sports" >sports</Menu.Item>
+                <Menu.Item key = "technology" >technology</Menu.Item>
+              </SubMenu>
+            </Menu>
+
+          </div>
+          <div className = 'content-main'>
+            {colorStyle.map((industry) => {
+              return industry.colorset.map((colorset) => {
+                return <ColorsetCard info = {colorset}/>
+              })
+            })}
+          </div>
+        </Content>
+        <Footer className = 'footer'>Copyright © 2021-2021 Siji Chen. All rights reserved.</Footer>
+      </Layout>
     </div>
-  );
+    )
+  };
 }
 
 export default App;
---
+++ src/ColorsetCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './ColorsetCard.css';
+
+export interface CardProps {
+    info: {
+        id: string;
+        rgbs: string[];
+        aesthetics: number;
+        distinctiveness: number;
+        specificity: number[];
+        specificities: object;
+    }
+}
+
+export interface CardState {
+
+}
+
+export default class ColorsetCard extends React.PureComponent<CardProps, CardState> {
+    // eslint-disable-next-line
+    constructor(props: CardProps) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className = 'colorset-card'>
+            {this.props.info.rgbs.map((rgb, index) => {
+                return <div className = {`colorset-unit ${index === 0 ? 'colorset-unit-first' : ''} ${index === (this.props.info.rgbs.length - 1) ? 'colorset-unit-last' : ''}`} style ={{backgroundColor: rgb}}></div>
+            })}
+            </div>
+        )
+    }
+}
