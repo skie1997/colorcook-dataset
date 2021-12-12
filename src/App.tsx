9069d8612f0448a2c0188c64bec8be7ecb0f0fd9
skie1997 <skie1997@outlook.com>
2021-12-12 18:05:16 +0800

    add rank&filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,114 @@
 import React from 'react';
 import { Layout, Menu, Checkbox } from 'antd';
-import { BulbOutlined, StockOutlined } from '@ant-design/icons';
+// import { BulbOutlined, StockOutlined } from '@ant-design/icons';
 import './App.css';
-import { colorStyle } from './assets/palettes/colorStyle_all_2_ok';
+import { colorStyle } from './assets/palettes/colorStyle_all_2_ok2';
 import ColorsetCard from './ColorsetCard';
-import SubMenu from 'antd/lib/menu/SubMenu';
-const { Header, Footer, Content } = Layout;
+// import SubMenu from 'antd/lib/menu/SubMenu';
+// import Sider from 'antd/lib/layout/Sider';
+const { SubMenu } = Menu;
+const { Sider, Header, Footer, Content } = Layout;
 const CheckboxGroup = Checkbox.Group;
 
 export interface AppState {
   industryFilter: string[];
   indeterminate: boolean;
   checkAll: boolean;
+  mode: string,
+  rank: string
 }
+
+const industryOperation = ['business', 'education', 'healthcare', 'finance', 'manufacturing', 'social', 'sports', 'technology'];
 
 class App extends React.Component<{}, AppState> {
   // eslint-disable-next-line
   constructor(props: any) {
       super(props);
       this.state = {
-        industryFilter: [],
+        industryFilter: ['business', 'education', 'healthcare', 'finance', 'manufacturing', 'social', 'sports', 'technology'],
         indeterminate: true,
         checkAll: false,
+        mode: 'light',
+        rank: 'aesthetics'
       }
 
   }
-  
-  onIndustryChange = (e: any) => {
-    console.log('e', e);
+
+  onRankChange = (e: any) => {
+    console.log(e)
     this.setState({
-      industryFilter: e.target.value
+      rank: e.key
+    })
+  }
+  
+  onIndustryChange = (industryFilter: any) => {
+    this.setState({
+      industryFilter: industryFilter,
+      indeterminate: !!industryFilter.length && industryFilter.length < industryOperation.length,
+      checkAll: industryFilter.length === industryOperation.length
     })
   }
 
+  onCheckAllChange = (e: any) => {
+    this.setState({
+      industryFilter: e.target.checked? industryOperation : [],
+      indeterminate: false,
+      checkAll: e.target.checked
+    })
+  }
+
+
   render() {
-    const industryOperation = ['business', 'education', 'healthcare', 'finance', 'manufacturing', 'social', 'sports', 'technology'];
-    const industryName = ['business', 'education', 'healthcare', 'finance', 'manufacturing', 'social media', 'sports', 'technology'];
-    const { industryFilter, indeterminate, checkAll } = this.state;
+    
+    // const defaultIndustryFilter = ['business', 'education', 'healthcare', 'finance', 'manufacturing', 'social media', 'sports', 'technology'];
+    const { industryFilter, indeterminate, checkAll, mode, rank} = this.state;
+    console.log('111', colorStyle.filter((colorset) => industryFilter.indexOf(colorset.industryName)!== -1).sort((a, b) => rank === 'aesthetics'? a.aesthetics - b.aesthetics : a.distinctiveness - b.distinctiveness))
     
     return(
     <div className="App">
       <Layout>
-        <Header className = 'header'>
+        <Header className = 'header' style={{height: 240, backgroundColor: '#FFF'}}>
           <h1 className = 'header-title'>Palettes Gallery Of ColorCook</h1>
-          <h3 className = 'header-description'>
+          <h3 className = 'header-description' style={{lineHeight: 2}}>
             We collected graphic designs(illustrations) that depict scenarios of the identified domains and extracted corresponding color palettes from these
             designs. We also validated the dataset from three aspects, including distinctiveness, aesthetics, and specificity. The
             final dataset contains 115 manually curated palettes and is released to support the design of future data-driven color
             authoring tools. 
           </h3>
         </Header>
-        <Content className = 'content'>
-          <div className = 'content-side'>
-            <Menu mode = "inline" openKeys = {['mode']} selectedKeys = {["dark"]} style = {{ height: '100%' }} >
-              <SubMenu className = 'content-side-submenu' key = "mode" title = "Mode" >
-                <Menu.Item key = "dark" >dark</Menu.Item>
-                <Menu.Item key = "light" >light</Menu.Item>
-              </SubMenu>
-            </Menu>
+        <Layout>
+          <Sider className = 'sider' width={'20%'} style={{background: 'white'}}>
+          <Menu mode = "inline" defaultOpenKeys = {['mode']} defaultSelectedKeys = {[mode]} style = {{ height: '100%' }} >
+                <SubMenu className = 'content-side-submenu' key = "mode" title = "Mode" >
+                  <Menu.Item key = "dark" >dark</Menu.Item>
+                  <Menu.Item key = "light" >light</Menu.Item>
+                </SubMenu>
+              </Menu>
 
-            <Menu mode = "inline" openKeys = {['rank']} style = {{ height: '100%' }} >
-              <SubMenu className = 'content-side-submenu' key = "rank" title = "Rank">
-                <Menu.Item key = "aesthetics" >aesthetics</Menu.Item>
-                <Menu.Item key = "distinctiveness" >distinctiveness</Menu.Item>
-              </SubMenu>
-            </Menu>
+              <Menu mode = "inline" defaultOpenKeys = {['rank']} defaultSelectedKeys = {[rank]} style = {{ height: '100%' }} onClick = {this.onRankChange}>
+                <SubMenu className = 'content-side-submenu' key = "rank" title = "Rank">
+                  <Menu.Item key = "aesthetics" >aesthetics</Menu.Item>
+                  <Menu.Item key = "distinctiveness" >distinctiveness</Menu.Item>
+                </SubMenu>
+              </Menu>
 
-            <Menu mode = "inline" openKeys = {['industry']} style = {{ height: '100%' }} >
-              <SubMenu className = 'content-side-submenu' key = "industry" title = "Industry">
-                {/* <Menu.Item key = "business" >business</Menu.Item>
-                <Menu.Item key = "education" >education</Menu.Item>
-                <Menu.Item key = "healthcare" >healthcare</Menu.Item>
-                <Menu.Item key = "finance" >finance</Menu.Item>
-                <Menu.Item key = "manufacturing" >manufacturing</Menu.Item>
-                <Menu.Item key = "social" >social media</Menu.Item>
-                <Menu.Item key = "sports" >sports</Menu.Item>
-                <Menu.Item key = "technology" >technology</Menu.Item> */}
-                <Checkbox indeterminate={indeterminate}>
-                  Check all
-                </Checkbox>
-                <CheckboxGroup options={industryOperation} value={industryName} onChange={this.onIndustryChange} />
-              </SubMenu>
-            </Menu>
-
-           
-
-          </div>
-          <div className = 'content-main'>
-            {colorStyle.map((industry) => {
-              return industry.colorset.map((colorset) => {
-                return <ColorsetCard info = {colorset} industryFilter = {this.state.industryFilter} />
-              })
-            })}
-          </div>
-        </Content>
+              <Menu mode = "inline" defaultOpenKeys = {['industry']} style = {{ height: '100%' }} >
+                <SubMenu className = 'content-side-submenu' key = "industry" title = "Industry">
+                  <Checkbox indeterminate={indeterminate} onChange={this.onCheckAllChange} checked={checkAll}>
+                    Check all
+                  </Checkbox>
+                  <CheckboxGroup options={industryOperation} value={industryFilter} onChange={this.onIndustryChange} />
+                </SubMenu>
+              </Menu>
+          </Sider>
+          <Content className = 'content'>
+            <div className = 'content-main'>
+              {colorStyle.filter((colorset) => industryFilter.indexOf(colorset.industryName)!== -1).sort((a, b) => rank === 'aesthetics'? a.aesthetics - b.aesthetics : a.distinctiveness - b.distinctiveness).map((colorset) => {
+                  return <ColorsetCard info = {colorset} industryFilter = {this.state.industryFilter} />
+                })}
+            </div>
+          </Content>
+        </Layout>
         <Footer className = 'footer'>Copyright © 2021-2021 Siji Chen. All rights reserved.</Footer>
       </Layout>
     </div>
--- src/ColorsetCard.tsx
+++ src/ColorsetCard.tsx
@@ -22,12 +22,35 @@ export default class ColorsetCard extends React.PureComponent<CardProps, CardSta
     constructor(props: CardProps) {
         super(props);
     }
+
+    enterColor = (e: any) => {
+     
+        // for(let i in e.target.parentElement.childNodes) {
+        //     console.log(e.target.parentElement.childNodes[i].style)
+        //     e.target.parentElement.childNodes[i].style.setProperty('display', 'flex');
+        //     e.target.parentElement.childNodes[i].style.setProperty('flex', '0');
+        // }
+        // let previousSibling = e.target.previousSibling;
+        // while(previousSibling) {
+        //     console.log('111')
+        //     e.target.previousSibling.style.flex = 0
+        //     previousSibling = previousSibling.previousSibling
+        // }
+
+        // let eles = document.querySelectorAll<HTMLElement>('.' + e.target.classList[1]);
+        // for(let i = 0; i < eles.length; i++) {
+        //     console.log('eles', eles[i])
+        //     eles[i]['style']['flex'] = 0;
+        // }
+        
+
+    }
     
     render() {
         return (
             <div className = 'colorset-card'>
             {this.props.info.rgbs.map((rgb, index) => {
-                return <div className = {`colorset-unit ${index === 0 ? 'colorset-unit-first' : ''} ${index === (this.props.info.rgbs.length - 1) ? 'colorset-unit-last' : ''}`} style ={{backgroundColor: rgb}}></div>
+                return <div className = {`colorset-unit ${index === 0 ? 'colorset-unit-first' : ''} ${index === (this.props.info.rgbs.length - 1) ? 'colorset-unit-last' : ''}` + ` ${this.props.info.id}`} style ={{backgroundColor: rgb}} onMouseEnter={this.enterColor}></div>
             })}
             </div>
         )
