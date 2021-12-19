10895885149ee890392e2edf60df7e593212c266
skie1997 <skie1997@outlook.com>
2021-12-20 02:44:54 +0800

    feat: add download as image

--- src/ColorsetCard.tsx
+++ src/ColorsetCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ColorsetCard.sass';
 import { Alert } from 'antd';
+import { FileImageOutlined, FileTextOutlined, EyeOutlined} from '@ant-design/icons';
+import html2canvas from 'html2canvas';
 
 export interface CardProps {
     info: {
@@ -46,18 +48,51 @@ export default class ColorsetCard extends React.PureComponent<CardProps, CardSta
             console.error('Failed to copy: ', err);
           }
     }
+
+    downImg = (e: any) => {
+        let ele = e.target.parentElement.parentElement.parentElement.parentElement.previousSibling;
+        html2canvas(ele).then((canvas) => {
+            let imgData = canvas.toDataURL("image/png");
+            let aLink = document.createElement("a");
+            aLink.style.display = "none";
+            aLink.href = imgData;
+            aLink.download = "colorset.png";
+            document.body.appendChild(aLink);
+            aLink.click();
+            document.body.removeChild(aLink);
+        })
+    }
     
     render() {
         return (
             <div className = 'colorset-card'>
-                {this.props.info.rgbs.map((rgb, index) => {
-                    const hex = this.rgbToHex(rgb);
-                    return (
-                        <div className = {`colorset-unit ${index === 0 ? 'colorset-unit-first' : ''} ${index === (this.props.info.rgbs.length - 1) ? 'colorset-unit-last' : ''}`} style ={{backgroundColor: rgb}} >
-                            <span className= 'colorset-value-span' onClick = {(e) => this.copyColor(e, hex)}>{hex}</span>
-                        </div>)
+                <div className = 'colorset'>
+                    {this.props.info.rgbs.map((rgb, index) => {
+                        const hex = this.rgbToHex(rgb);
+                        return (
+                            <div className = {`colorset-unit ${index === 0 ? 'colorset-unit-first' : ''} ${index === (this.props.info.rgbs.length - 1) ? 'colorset-unit-last' : ''}`} style ={{backgroundColor: rgb}} >
+                                <span className= 'colorset-value-span' onClick = {(e) => this.copyColor(e, hex)}>{hex}</span>
+                            </div>)
 
-                })}
+                    })}
+                </div>
+                <div className = 'download'>
+                    <div className = 'downImg'>
+                        <div className = 'downImg-back'>
+                            <FileImageOutlined style = {{fontSize: 15, color: '#222'}} onClick = {this.downImg}/>
+                        </div>
+                    </div>
+                    <div className = 'downJson' >
+                        <div className = 'downJson-back'>
+                            <FileTextOutlined style = {{fontSize: 15, color: '#222'}}/>
+                        </div>
+                    </div>
+                    <div className = 'viewImg' >
+                        <div className = 'viewImg-back'>
+                            <EyeOutlined style = {{fontSize: 15, color: '#222'}}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
